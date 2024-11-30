d142002868682369b2f7be71c2e546de07416a72
hiktan44 <140920746+hiktan44@users.noreply.github.com>
2024-11-30 08:09:59 +0300

    Update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,28 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#60A5FA',
+          dark: '#3B82F6',
+        },
+        background: {
+          light: '#FFFFFF',
+          dark: '#1F2937',
+        },
+        surface: {
+          light: '#F3F4F6',
+          dark: '#374151',
+        },
+        text: {
+          light: '#1F2937',
+          dark: '#F9FAFB',
+        }
+      }
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-};
+  plugins: [],
+}
